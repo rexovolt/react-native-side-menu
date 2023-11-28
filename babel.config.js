f2cbc04729f110739f96e25b343d186c155c3745
Rexogamer <beartechtalks@gmail.com>
2023-11-28 09:18:28 +0000

    chore!: drop Flow support/package upgrades

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  env: {
+    production: {
+      plugins: [
+        [
+          'transform-remove-console',
+          {
+            exclude: ['error', 'warn'],
+          },
+        ],
+      ],
+    },
+  },
+};
